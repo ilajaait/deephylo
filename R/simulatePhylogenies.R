25311d33e2d0fe71285a02bcc3d90af88700c431
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-07 17:02:05 +0200

    ✨ Simulate CRBD phylo

--- R/simulatePhylogenies.R
+++ R/simulatePhylogenies.R
@@ -1,4 +1,4 @@
-#' Create a phylogeny under the Constant Rate Birth-Death (CRBD) model
+#' Create one phylogeny under the Constant Rate Birth-Death (CRBD) model
 #'
 #' @param n.tips number of tips
 #' @param lambda speciation rate
@@ -18,7 +18,7 @@ createPhyloCRBD <- function(n.tips, lambda, mu) {
   )[[1]]
 }
 
-#' Create a phylogeny under the BiSSE model
+#' Create one phylogeny under the BiSSE model
 #'
 #' The BiSSE diversification model has 6 parameters:
 #'    * \eqn{\lambda_0} speciation rate of state 0
@@ -46,5 +46,93 @@ createPhyloCRBD <- function(n.tips, lambda, mu) {
 #' q <- 0.1 # transition rate
 #' createPhyloBiSSE(n.tips, lambda0, q)
 createPhyloBiSSE <- function(n.tips, lambda0, q) {
-  diversitree::tree.bisse(c(lambda0,2*lambda0,0,0,q,q), max.taxa = n.tips)
+  diversitree::tree.bisse(c(lambda0, 2 * lambda0, 0, 0, q, q), max.taxa = n.tips)
+}
+
+#' Simulate phylogenies under the CRBD model
+#'
+#' Generate a list of phylogenies under the Constant Rate Birth Death (CRBD)
+#' model. Can check and save summary statistics of each phylogeny by activating
+#' `sumstat_check`. Moreover true parameters of each phylogeny are also
+#' returned.
+#'
+#' @param n.phylo number of phylogenies to simulate
+#' @param tips.lim phylogeny size range
+#' @param lambda.lim speciation rate range
+#' @param sumstat_check whether to check that summary statistics can be computed
+#'    for each phylogeny and save them (logical)
+#' @param verbose whether to print progress (logical)
+#'
+#' @return list
+#'    * `$phylo`: list of simulated phylogenies (ape format)
+#'    * `$params`: dataframe of phylogeny parameters
+#'    * `$sumstat`: dataframe of phylogeny summary statistics
+#'    (empty if `ss_check = FALSE`)
+#'
+#' @export
+#'
+#' @examples simulatePhyloCRBD(10, c(50, 100), c(0.1, 1.)) # simulate 10 phylo
+simulatePhyloCRBD <- function(n.phylo, tips.lim, lambda.lim,
+                              sumstat_check = T, verbose = F) {
+
+  # Tests
+  length(tips.lim) == 2 || stop("tips.lim should be of length 2.")
+  length(lambda.lim) == 2 || stop("lambda.lim should be of length 2.")
+
+  # Set up
+  phylo.list <- list() # store simulated phylogenies
+  params.df <- data.frame(lambda = rep(NA, n.phylo), mu = rep(NA, n.phylo))
+  sumstat.list <- list()
+
+  # Simulation loop
+  while (length(phylo.list) < n.phylo) {
+    params <- drawParamCRBD(lambda.lim)
+    n.tips <- drawPhyloSize(tips.lim)
+    phylo <- createPhyloCRBD(n.tips, params$lambda, params$mu)
+    if (sumstat_check) {
+      sumstat <- sumStats(phylo)
+      if (all(!is.na(sumstat))) {
+        phylo.list <- append(phylo.list, list(phylo))
+        sumstat.list <- append(sumstat.list, list(sumstat))
+        params.df[length(phylo.list), ] <- params
+      }
+    } else {
+      phylo.list <- append(phylo.list, list(phylo))
+      params.df[length(phylo.list), ] <- params
+    }
+    if (verbose) {
+      svMisc::progress(length(phylo.list), n.phylo,
+        progress.bar = TRUE,
+        init = (length(phylo.list) == 1)
+      )
+    }
+  }
+
+  # Output
+  sumstat.df <- as.data.frame(do.call(rbind, sumstat.list))
+  list(phylo = phylo.list, params = params.df, sumstat = sumstat.df)
+}
+
+#' Draw parameters for the CRBD model
+#'
+#' To ensure that \eqn{\lambda >> \mu}, parameters are drawn as follow:
+#'    * \eqn{\lambda} in (`min(lambda.lim)`, `max(lambda.lim)`)
+#'    * \eqn{\mu} in (0, \eqn{0.9 \lambda})
+#'
+#' @param lambda.lim speciation rate boundaries (vector)
+#'
+#' @return list of parameters
+drawParamCRBD <- function(lambda.lim) {
+  lambda <- stats::runif(1, lambda.lim[1], lambda.lim[2])
+  mu <- stats::runif(1, 0, 0.9 * lambda)
+  list(lambda = lambda, mu = mu)
+}
+
+#' Draw size of a phylogeny
+#'
+#' @param tips.lim size boundaries (vector)
+#'
+#' @return int
+drawPhyloSize <- function(tips.lim) {
+  round(stats::runif(1, tips.lim[1], tips.lim[2]))
 }
--- tests/testthat/test-simulatePhylogenies.R
+++ tests/testthat/test-simulatePhylogenies.R
@@ -16,3 +16,26 @@ test_that("Create phylogeny correctly.", {
   expect_false(all(phylo$tip.state == 1) | all(phylo$tip.state == 0))
 })
 
+test_that("Draw parameters correctly.", {
+
+  # Phylo size
+  expect_equal(drawPhyloSize(c(10, 10)), 10)
+  size <- drawPhyloSize(c(10, 20))
+  expect_true(size >= 10 & size <= 20)
+
+  # Rate CRBD
+  expect_equal(list(lambda = 0, mu = 0), drawParamCRBD(c(0, 0)))
+  params <- drawParamCRBD(c(0, 1))
+  expect_true(params$lambda > params$mu)
+  expect_true(params$lambda > 0 & params$mu > 0)
+  expect_true(params$lambda < 1 & params$mu < 1)
+})
+
+
+test_that("Simulate phylogeny correctly.", {
+  phylo.list <- simulatePhyloCRBD(10, c(20, 50), c(0.1, 1.0))
+  expect_equal(length(phylo.list), 3)
+  expect_equal(length(phylo.list$phylo), 10)
+  expect_equal(nrow(phylo.list$params), 10)
+  expect_equal(ncol(phylo.list$params), 2)
+})
